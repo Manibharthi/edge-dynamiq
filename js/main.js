5a8e9efbd4a7313ba325e7ceab14533ea24d0301
prvnkmr679 <prvnkmr679@gmail.com>
2024-10-20 20:44:38 +0530

    new17 sa

--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@ document.querySelector('.custom-toggler').addEventListener('click', function() {
 });
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.arrow-btn');
-    buttons.forEach(button => {
-      button.addEventListener('click', function () {
-        const targetId = button.getAttribute('data-target');
-        const targetSection = document.querySelector(targetId);
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.arrow-btn');
+  buttons.forEach(button => {
+    button.addEventListener('click', function () {
+      const targetId = button.getAttribute('data-target');
+      const targetSection = document.querySelector(targetId);
 
-        // Close all other open service sections
-        document.querySelectorAll('.service-details').forEach(section => {
-          if (section !== targetSection) {
-            section.style.display = 'none';
-          }
-        });
-
-        // Toggle the selected section
-        if (targetSection.style.display === 'none') {
-          targetSection.style.display = 'block';
-        } else {
-          targetSection.style.display = 'none';
+      // Close all other open service sections
+      document.querySelectorAll('.service-details').forEach(section => {
+        if (section !== targetSection) {
+          section.style.display = 'none';
         }
       });
+
+      // Toggle the selected section
+      if (targetSection.style.display === 'none') {
+        targetSection.style.display = 'block';
+      } else {
+        targetSection.style.display = 'none';
+      }
     });
   });
+});
 
